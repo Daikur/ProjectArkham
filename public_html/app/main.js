f3d46695519caeb2036622552627c294592b233f
Surfer96 <borjaa96@hotmail.com>
2017-01-08 18:29:19 +0100

    Prueba conection

---
+++ public_html/app/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    console.log("Hola ");
+
+    $.ajax({
+        dataType: 'json',
+        type: 'GET',
+        url: 'http://localhost/slim/api.php/categorias',
+        success: function (data){
+            $.each(data, function (i){
+                console.log(data[i]);
+            });
+
+
+
+        }
+
+
+
+    });
+
+
+});
